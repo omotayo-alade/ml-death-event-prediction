1f404b38377eae647388de0486dfbb481d8d26a5
omotayo-alade <aladeomotayoseye@gmail.com>
2020-06-21 01:35:29 +0100

    'update file'

--- model.py
+++ model.py
@@ -13,15 +13,16 @@ dataset = pd.read_csv('processed_data.csv')
 x = dataset[['creatinine_phosphokinase', 'ejection_fraction', 'serum_creatinine', 'time']]
 y = dataset['death_event']
 
+x = PowerTransformer().fit(x).transform(x)
+
 # Splitting dataset into training and testing data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-# Creating pipeline
-steps = [('transformer', PowerTransformer()),
-         ('model', LogisticRegression(solver='liblinear', penalty='l2', C=1.0, max_iter=200, random_state=0))]
+# Creating model
+model = LogisticRegression(solver='liblinear', penalty='l2', C=1.0, max_iter=200, random_state=0)
 
 # Fitting the model with training data
-classifier = Pipeline(steps).fit(x_train, y_train)
+classifier = model.fit(x_train, y_train)
 
 # Saving model to disk
 pickle.dump(classifier, open('model.pkl','wb'))
