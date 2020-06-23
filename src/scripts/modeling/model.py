ce68c5d844f9f64f413d6ae98710117feba1ae5b
omotayo-alade <aladeomotayoseye@gmail.com>
2020-06-23 22:07:54 +0100

    re-evaluate model

--- src/scripts/modeling/model.py
+++ src/scripts/modeling/model.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.preprocessing import PowerTransformer
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
     
 # Loading dataset
@@ -15,10 +15,10 @@ y = dataset['death_event']
 x = PowerTransformer().fit_transform(x)
 
 # Splitting dataset into training and testing data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
 # Creating model
-model = LogisticRegression(solver='liblinear', penalty='l1', C=0.1, max_iter=200)
+model = SVC(kernel='linear', C=1, random_state=1)
 
 # Fitting the model with training data
 classifier = model.fit(x_train, y_train)
