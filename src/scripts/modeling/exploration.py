2d5ddb5f48e9b43da50ac315d9d33a364346aa21
omotayo-alade <aladeomotayoseye@gmail.com>
2020-06-24 23:20:56 +0100

    add visualization

--- src/scripts/modeling/exploration.py
+++ src/scripts/modeling/exploration.py
@@ -22,74 +22,94 @@ import numpy as np
 df = pd.read_csv('../../data/processed/processed_data.csv')
 df.head()
 
-df.corr()['death_event'][:-1].sort_values(ascending=False)
+df.columns = ['Age', 'Anaemia', 'CP', 'Diabetes', 'EF', 'HBP',
+              'Platelets','SC', 'SS', 'Gender', 'Smoking', 'Time', 'Death_Event']
+
+df.corr()['Death_Event'][:-1].sort_values(ascending=False)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 from yellowbrick.target import FeatureCorrelation
 
-x = df.drop('death_event', axis=1)
-y = df['death_event']
+x = df.drop('Death_Event', axis=1)
+y = df['Death_Event']
 visual = FeatureCorrelation(method='pearson', label=x.columns, sort=True).fit(x,y)
 visual.poof();
 
+fig, ax = plt.subplots(figsize=(15,8))
+sns.heatmap(df.corr(), annot=True, square=False, ax=ax)
+plt.show()
+
 df.head(2)
 
-df['anaemia'].replace({0:'False', 1:'True'}, inplace=True)
-df['diabetes'].replace({0:'False', 1:'True'}, inplace=True)
-df['high_blood_pressure'].replace({0:'False', 1:'True'}, inplace=True)
-df['sex'].replace({0:'Female', 1:'Male'}, inplace=True)
-df['smoking'].replace({0:'False', 1:'True'}, inplace=True)
+df['Anaemia'].replace({0:'False', 1:'True'}, inplace=True)
+df['Diabetes'].replace({0:'False', 1:'True'}, inplace=True)
+df['HBP'].replace({0:'False', 1:'True'}, inplace=True)
+df['Gender'].replace({0:'Female', 1:'Male'}, inplace=True)
+df['Smoking'].replace({0:'False', 1:'True'}, inplace=True)
+df['Death_Event'].replace({0:'Survived', 1:'Died'}, inplace=True)
 
-bins = np.linspace(df['age'].min(), df['age'].max(), 5, dtype=('int'))
+bins = np.linspace(df['Age'].min(), df['Age'].max(), 5, dtype=('int'))
 labels = ['40-53', '53-67', '67-81', '81-95']
-df['age_binned'] = pd.cut(df['age'], bins=bins, labels=labels, include_lowest=True)
-df['age_binned'].unique()
+df['Age_Group'] = pd.cut(df['Age'], bins=bins, labels=labels, include_lowest=True)
+df['Age_Group'].unique()
 
-age_grouped = df.groupby('age_binned')[['creatinine_phosphokinase',
-                                    'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium','time']].sum()
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+age_grouped = df.groupby('Age_Group')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
 age_grouped
 
-anaemia_grouped = df.groupby('anaemia')[['creatinine_phosphokinase',
-                                   'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium', 'time']].mean()
-anaemia_grouped.head()
+anaemia_grouped = df.groupby('Anaemia')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
+anaemia_grouped
 
-diabetes_grouped = df.groupby('diabetes')[['creatinine_phosphokinase',
-                                   'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium', 'time']].mean()
-diabetes_grouped.head()
+diabetes_grouped = df.groupby('Diabetes')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
+diabetes_grouped
 
-bloodP_grouped = df.groupby('high_blood_pressure')[['creatinine_phosphokinase',
-                                   'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium', 'time']].mean()
-bloodP_grouped.head()
+HBP_grouped = df.groupby('HBP')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
+HBP_grouped
 
-sex_grouped = df.groupby('sex')[['creatinine_phosphokinase',
-                                   'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium', 'time']].mean()
-sex_grouped.head()
+sex_grouped = df.groupby('Gender')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
+sex_grouped
 
-smoking_grouped = df.groupby('smoking')[['creatinine_phosphokinase',
-                                   'ejection_fraction', 'platelets',
-                                   'serum_creatinine', 'serum_sodium', 'time']].mean()
-smoking_grouped.head()
+smoking_grouped = df.groupby('Smoking')[['CP', 'EF', 'Platelets', 'SC', 'SS', 'Time']].mean()
+smoking_grouped
 
-table = pd.crosstab(df['age_binned'], df['anaemia'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['Anaemia'])
+table.plot(kind='bar')
 
-table = pd.crosstab(df['age_binned'], df['diabetes'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['Diabetes'])
+table.plot(kind='bar')
 
-table = pd.crosstab(df['age_binned'], df['high_blood_pressure'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['HBP'])
+table.plot(kind='bar')
 
-table = pd.crosstab(df['age_binned'], df['sex'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['Gender'])
+table.plot(kind='bar')
 
-table = pd.crosstab(df['age_binned'], df['smoking'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['Smoking'])
+table.plot(kind='bar')
 
-table = pd.crosstab(df['age_binned'], df['death_event'])
-table.head()
+table = pd.crosstab(df['Age_Group'], df['Death_Event'])
+table.plot(kind='bar')
+
+table = pd.crosstab(df['Anaemia'], df['Death_Event'])
+table.plot(kind='bar')
+
+table = pd.crosstab(df['Diabetes'], df['Death_Event'])
+table.plot(kind='bar')
+
+table = pd.crosstab(df['HBP'], df['Death_Event'])
+table.plot(kind='bar')
+
+table = pd.crosstab(df['Gender'], df['Death_Event'])
+table.plot(kind='bar')
+
+table = pd.crosstab(df['Smoking'], df['Death_Event'])
+table.plot(kind='bar')
+
+sns.barplot(df['Age_Group'], df['Time'], errwidth=0)
+
+pd.crosstab(df['Age_Group'], df['Death_Event'])
+
+
